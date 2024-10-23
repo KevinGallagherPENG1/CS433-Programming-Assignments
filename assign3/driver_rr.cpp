6fc517cb7f1f4d9c8b495cef355e34be24f10aaf
KevinGallagherPENG1 <100745126+KevinGallagherPENG1@users.noreply.github.com>
2024-10-23 13:28:20 -0700

    Fixed a stupid fucking error (extraneous space).

--- assign3/driver_rr.cpp
+++ assign3/driver_rr.cpp
@@ -62,7 +62,7 @@ int main(int argc, char *argv[]) {
     }
 
     // Create a scheduler object
-    SchedulerRR scheduler (time_quantume);
+    SchedulerRR scheduler(time_quantume);
     // Run the scheduler
     scheduler.init(process_list);
     scheduler.simulate();
